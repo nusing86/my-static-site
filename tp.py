1a3a341bd8418d1ea18e896105a1fdc69153ad5e
nusing86 <151864321+nusing86@users.noreply.github.com>
2023-11-24 01:26:20 +0800

    templates

---
+++ tp.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    image_folder = os.path.join('static', 'images')
+    images = [os.path.join(image_folder, image) for image in os.listdir(image_folder)]
+    return render_template('gallery.html', images=images)
+
+if __name__ == '__main__':
+    app.run(debug=True)
