a6bd016c5e951c02f1d23e27b358787eacc70e73
nusing86 <151864321+nusing86@users.noreply.github.com>
2023-11-24 03:24:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const images = ['法师_000001', '法师_000130', /* 更多图片文件名 */];
+const images = ['法师_000001.jpg', '法师_000130.jpg', /* 更多图片文件名 */];
 const gallery = document.getElementById('gallery');
 
 images.forEach(image => {
