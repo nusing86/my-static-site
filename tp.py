a2f1c4a55958c18f9065d7e6f6c2c35446e84958
nusing86 <151864321+nusing86@users.noreply.github.com>
2023-11-24 02:04:11 +0800

    Update tp.py

--- tp.py
+++ tp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     image_folder = os.path.join('static', 'images')
-    images = [os.path.join(image_folder, image) for image in os.listdir(image_folder)]
+    images = [os.path.join(image_folder, image) for image in os.listdir(image_folder) if image.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
     return render_template('gallery.html', images=images)
 
 if __name__ == '__main__':
